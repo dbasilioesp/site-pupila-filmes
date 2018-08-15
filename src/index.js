714cdd12ce7e3e0d4535ff6b90bd61ed1776de97
David Basilio <dbasilioesp@gmail.com>
2018-08-15 15:56:07 -0300

    Saving the assets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import 'normalize.css';
 import '../assets/scss/main.scss';
+// import 'script-loader!gsap/src/uncompressed/TweenMax';
+// import 'script-loader!scrollmagic';
+// import 'script-loader!scrollmagic/scrollmagic/minified/plugins/animation.gsap.min';
 
-console.log('GO');
+// var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "100%" } });
+
+// new ScrollMagic.Scene({ triggerElement: "#section-tellmemore" })
+//   .setTween("#section-tellmemore > #parallax1", { y: "-20%" })
+//   .addTo(controller);
