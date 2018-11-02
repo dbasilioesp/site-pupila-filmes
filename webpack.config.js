205db564c6658c7bd58a4556dc5b6a1470cf4158
dbasilioesp <dbasilioesp@gmail.com>
2018-11-02 00:36:54 -0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -10,15 +10,37 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
-      { test: /\.css$/, use: ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader" }) },
-      { test: /\.scss$/, use: ExtractTextPlugin.extract({ fallback: "style-loader", use: ["css-loader", "sass-loader"] }) },
-      { test: /\.pug$/, use: [{ loader: 'pug-loader', options: { pretty: true } }] }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      { test: /\.(png|svg|jpg|gif)$/, use: "file-loader" },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: "file-loader" },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.pug$/,
+        use: [{ loader: "pug-loader", options: { pretty: true } }]
+      }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css'),
-    new HtmlWebpackPlugin({ template: 'index.pug' })
-  ],
+    new ExtractTextPlugin("style.css"),
+    new HtmlWebpackPlugin({ template: "index.pug" })
+  ]
 };
